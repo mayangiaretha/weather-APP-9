bba46f2133aee6eb2573b7250d9fb1a81856ff44
ceeygithub <cynthiaekhomwandolor@gmail.com>
2024-01-11 03:56:20 +0100

    made changes

--- JS/index.js
+++ JS/index.js
@@ -1,59 +1,8 @@
 
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   const input = document.querySelector('#cityInput');
-//   const button = document.querySelector('#Btn');
-
-
-//   async function submitButton(city) {
-//     // event.preventDefault();
-
-//     const inputValue = input.value;
-
-
-//     const apiKey = "597c40c39084687093b091cd48b366f8";
-//     // const city = inputValue;
-
-//     try {
-//       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}&units=metric`);
-//       const data = await response.json();
-//       console.log(data, "this is the data===============>")
-//       const iconUrl = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-
-
-//       //     const weatherDataContainer = document.getElementById('weatherData');
-//       //     weatherDataContainer.innerHTML = `
-//       //       <h2>${data.name}, ${data.sys.country}</h2>
-//       //  <p>Temperature: ${Math.round(data.main.temp)}°C</p>
-//       //       <p>Weather: ${data.weather[0].description}</p>
-//       //       <img src="${iconUrl}" alt="Weather Icon">
-
-
-//       //     `;
-//       document.querySelector('.city').innerHTML = data.name;
-//       document.querySelector('.temp').innerHTML = Math, round(data.main.temp) + "℃";
-//       document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
-//       document.querySelector('.wind').innerHTML = data.wind.speed + " km/h";
-//     } catch (error) {
-//       console.error('Error fetching weather data:', error);
-//     }
-//   }
-
-//   // button.addEventListener('click', submitButton);
-//   button.addEventListener('click', () => {
-//     submitButton(input.value)
-//   });
-// });
-
-
-
-// 
-
 document.addEventListener('DOMContentLoaded', async function () {
   const input = document.querySelector('#cityInput');
   const button = document.querySelector('#Btn');
-  // const weatherData = document.querySelector('#weatherData')
-  // const error404 = document.querySelector('.not-found')
 
 
   async function submitButton(event) {
@@ -66,20 +15,14 @@ document.addEventListener('DOMContentLoaded', async function () {
       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}&units=metric`);
       const data = await response.json();
       updateWeatherDisplay(data);
-      // if (data.cod === '404') {
-      //   // Handle 404 error
-      //   weatherData.classList.remove('active');
-      //   error404.classList.add('active');
-      // } else {
-      //   // Display weather data
-      //   weatherData.classList.add('active');
-      //   error404.classList.remove('active');
-      //   updateWeatherDisplay(data);
-      // }
-
 
     } catch (error) {
       console.error('Error fetching weather data:', error);
+      weatherData.innerHTML = `<div class="not-found">
+      <img src="./assets/3828537.jpg" alt="" width="100px">
+      <h4 style="color:red">City not found!, please enter correct city</h4>
+      </div>`;
+
     }
   }
 
@@ -104,8 +47,7 @@ document.addEventListener('DOMContentLoaded', async function () {
     document.querySelector('.icon').src = iconUrl;
     document.querySelector('.city').innerHTML = data.name;
     document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "℃";
-    // document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
-    // document.querySelector('.wind').innerHTML = data.wind.speed + " km/h";
+
     document.querySelector('.humidity').innerHTML = `<i class="fa-solid fa-water"></i> Humidity: ${data.main.humidity}%`;
     document.querySelector('.wind').innerHTML = `<i class="fa-solid fa-wind"></i> Wind: ${data.wind.speed} km/h`;
   }
